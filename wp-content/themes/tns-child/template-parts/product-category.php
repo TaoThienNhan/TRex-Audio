31550049bad81e9590f4068914a6949fedc91b20
tuongkhac202 <87599130+tuongkhac202@users.noreply.github.com>
2023-08-30 20:23:41 +0700

    [Template-parts] UPDATE product-category.php

--- wp-content/themes/tns-child/template-parts/product-category.php
+++ wp-content/themes/tns-child/template-parts/product-category.php
@@ -1,17 +1,14 @@
 <style>
     .product-single {
         transition: top all .3s;
-        background: #4c4c4c;
-        opacity: 1;
     }
 
     .product-single-container:hover .product-single {
         background: url('<?= get_stylesheet_directory_uri(); ?>/assets/src/images/bg-single-product.png');
         background-size: 100% 100%;
-        background-position: top;
-        background-repeat: no-repeat;
     }
-    .brand-name{
+
+    .brand-name {
         font-weight: 100 !important;
     }
 </style>
@@ -21,7 +18,7 @@
         <div class="container">
             <div class="row py-3">
                 <div class="col-lg-4 category_name">
-                <h2 class="text-center py-3 m-0" style="background: url(<?= get_stylesheet_directory_uri(); ?>/assets/src/images/background-gold.png);background-position: center;font-size: 16px;font-weight: 800;">LOA</h2>
+                    <h2 class="text-center py-3 m-0" style="background: url(<?= get_stylesheet_directory_uri(); ?>/assets/src/images/background-gold.png);background-position: center;">LOA</h2>
                 </div>
                 <div class="text-white row col-lg-6 offset-lg-2 d-flex justify-content-between align-items-center my-2 my-lg-0">
                     <a href="" class="col text-white border-s brand-name">JBL</a> |
@@ -45,7 +42,7 @@
                                     <div class="col-lg-12">
                                         <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/logo.png" alt="" class="logo m-2 img-fluid">
                                         <strong class="bg-danger text-white py-2 px-4 ">HOT</strong>
-                                        <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/loa<?= $i ?>.png" alt="" class="w-100 img-fluid">
+                                        <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/loa<?= $i ?>.png" alt="" class="w-100 img-fluid product-img">
                                     </div>
                                 </div>
                                 <div class="row product-single-footer mx-0 d-flex align-items-center p-3">
