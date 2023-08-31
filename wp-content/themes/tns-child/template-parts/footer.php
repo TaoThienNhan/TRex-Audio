355de2b30716e648d075d92a504cc974d93a9f87
tuongkhac202 <87599130+tuongkhac202@users.noreply.github.com>
2023-08-31 14:12:07 +0700

    [Template_parts} _footer.php

---
+++ wp-content/themes/tns-child/template-parts/footer.php
@@ -0,0 +1,126 @@
+<style>
+    .footer {
+        background: url('<?= get_stylesheet_directory_uri(); ?>/assets/src/images/bg-footer.png');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
+
+<div class="footer py-5 bg-dark">
+    <div class="container">
+        <div class="row mb-4 ">
+            <div class="col-sm-4 d-flex align-items-center">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/logo-tranparent.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-sm-8 d-flex justify-content-sm-end align-items-center">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/dadangky.png" alt="" class="img-fluid my-3">
+            </div>
+            
+            <div class="col-md-4 pt-1 my-3">
+                <div class="location-container">
+                    <div class="location d-flex justify-content-start align-items-center">
+                        <i class="fas fa-map-marker-alt fs-3 text-white"></i>
+                        <div class="text-location ms-3">
+                            <p class=" mb-0">CẦN THƠ</p>
+                            <p class="text-white mb-0">Số 354 Võ Văn Kiệt, An Thới, Bình Thủy, Cần Thơ. </p>
+                        </div>
+                    </div>
+
+                    <div class="location d-flex justify-content-start align-items-center">
+                        <i class="fas fa-map-marker-alt fs-3 text-white"></i>
+                        <div class="text-location ms-3">
+                            <p class="mb-0 ">TP.HCM</p>
+                            <p class="text-white mb-0">161 Chu Văn An, Phường 26, Quận Bình Thạnh, TPHCM </p>
+                        </div>
+                    </div>
+
+                    <div class="location d-flex justify-content-start align-items-center">
+                        <i class="fas fa-map-marker-alt fs-3 text-white"></i>
+                        <div class="text-location ms-3">
+                            <p class="mb-0 ">NHA TRANG</p>
+                            <p class="text-white mb-0">20 Bến Cá, Nha Trang, Khánh Hòa </p>
+                        </div>
+                    </div>
+
+                    <div class="location d-flex justify-content-start align-items-center">
+                        <i class="fas fa-map-marker-alt fs-3 text-white"></i>
+                        <div class="text-location ms-3">
+                            <p class="mb-0 ">CẦN THƠ</p>
+                            <p class="text-white mb-0">400 Nguyễn Hữu Thọ, Mai Dịch, Hà Nội </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8 col-lg-7 offset-lg-1 my-3">
+                <div class="d-block d-md-flex justify-content-between">
+                    <div class="contact-footer pt-1">
+                        <div class="d-flex">
+                            <div class="contact-footer-left text-location">
+                                <p class="mb-0 me-3">HỖ TRỢ KĨ THUẬT: </p>
+                                <p class="mb-0 me-3">TƯ VẤN ĐẮT HÀNG: </p>
+                                <p class="mb-0 me-3">HOTLINE1: </p>
+                                <p class="mb-0 me-3">HOTLINE2: </p>
+                            </div>
+
+                            <div class="contact-footer-right text-location">
+                                <p class="text-white mb-0"> 0985.86.87.86</p>
+                                <p class="text-white mb-0"> 0985.86.87.65 </p>
+                                <p class="text-white mb-0"> 0393.65.33.65</p>
+                                <p class="text-white mb-0"> 0963.65.33.65</p>
+                            </div>
+                        </div>
+                        <p class="text-light mb-0">Email: t-rexaudio@gmail.com</p>
+                    </div>
+
+                    <div class="rules">
+                        <strong class="pb-1">ĐIỀU KHOẢN SỬ DỤNG</strong>
+                        <div>
+                            <p class="text-light mb-0">Chính sách bảo hành</p>
+                            <p class="text-light mb-0">Chính sách đổi trả</p>
+                            <p class="text-light mb-0">Phương thức giao nhận</p>
+                            <p class="text-light mb-0">Quyền riêng tư</p>
+                            <p class="text-light mb-0">Phương thức thanh toán</p>
+                            <p class="text-light mb-0">Hướng dẫn lắp đặt</p>
+                        </div>
+                    </div>
+
+                    <div class="nav-footer">
+                        <strong class="pb-1">TRANG CHỦ</strong>
+                        <div>
+                            <a href="">
+                                <p class="text-light mb-0">Loa</p>
+                            </a>
+                            <a href="">
+                                <p class="text-light mb-0">Cục dây</p>
+                            </a>
+                            <a href="">
+                                <p class="text-light mb-0">Micro</p>
+                            </a>
+                            <a href="">
+                                <p class="text-light mb-0">Mixed</p>
+                            </a>
+                            <a href="">
+                                <p class="text-light mb-0">Karaoke</p>
+                            </a>
+                            <a href="">
+                                <p class="text-light mb-0">Amply</p>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="row footer-bottom py-4">
+            <div class="col-lg-6 d-flex justify-content-center justify-content-lg-start">
+                <a href=""> <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/f.png" alt="" class="img-fluid me-2 me-lg-3"></a>
+                <a href=""> <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/google.png" alt="" class="img-fluid me-2 me-lg-3"></a>
+                <a href=""> <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/yu.png" alt="" class="img-fluid me-2 me-lg-3"></a>
+                <a href=""> <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/zalo.png" alt="" class="img-fluid me-2 me-lg-3"></a>
+            </div>
+            <div class="col-lg-6 mt-3 mt-lg-0 text-uppercase text-light text-center text-lg-end text-design-by">
+            <?php echo date('Y'); ?> © <a href="" class="text-light ">T-REX AUDIO</a> All Rights Reserved. Designed by <a href="" class="text-light ">Tay Nam Solution.</a> 
+            </div>
+        </div>
+    </div>
+</div>
