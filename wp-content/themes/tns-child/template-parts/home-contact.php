e0e77cc74b658f0e548617ef3c17d491d8c4ecba
tuongkhac202 <87599130+tuongkhac202@users.noreply.github.com>
2023-09-05 11:28:15 +0700

    [Template_parts} _home-contact.php

---
+++ wp-content/themes/tns-child/template-parts/home-contact.php
@@ -0,0 +1,61 @@
+<style>
+    .contact {
+        background: url('<?= get_stylesheet_directory_uri(); ?>/assets/src/images/bg-contact.png');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
+
+<div class="brand-logo bg-dark py-3">
+    <div class="container">
+        <div class="row gy-4 brand-banner py-3 py-md-4 d-flex justify-content-between align-items-center">
+
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/brand1.png" alt="" class="img-fluid col-6 col-md-4 col-lg-2">
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/brand2.png" alt="" class="img-fluid col-6 col-md-4 col-lg-2">
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/brand3.png" alt="" class="img-fluid col-6 col-md-4 col-lg-2">
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/brand4.png" alt="" class="img-fluid col-6 col-md-4 col-lg-2">
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/brand5.png" alt="" class="img-fluid col-6 col-md-4 col-lg-2">
+            <img src="<?= get_stylesheet_directory_uri(); ?>/assets/src/images/brand6.png" alt="" class="img-fluid col-6 col-md-4 col-lg-2">
+
+        </div>
+    </div>
+</div>
+
+<div class="home-contact py-5 bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="row">
+                    <!-- Video lớn ở trên -->
+                    <div class="col-12 border border-1 border-secondary p-2 d-flex justify-content-center align-items-lg-center big-video">
+                        <div class="responsive-iframe">
+                            <iframe src="https://www.youtube.com/embed/erjMgola4fQ?si=PIhHDjHY-VDhNUY4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row border border-1 border-secondary small-video">
+                    <!-- Video nhỏ ở dưới -->
+                    <div class="col-4 border border-1 border-secondary p-2 d-flex justify-content-center align-items-lg-center">
+                        <div class="responsive-iframe">
+                            <iframe src="https://www.youtube.com/embed/erjMgola4fQ?si=PIhHDjHY-VDhNUY4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="col-4 border border-1 border-secondary p-2 d-flex justify-content-center align-items-lg-center">
+                        <div class="responsive-iframe">
+                            <iframe src="https://www.youtube.com/embed/VIDEO_ID?enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div class="col-4 border border-1 border-secondary p-2 d-flex justify-content-center align-items-lg-center">
+                        <div class="responsive-iframe">
+                            <iframe src="https://www.youtube.com/embed/VIDEO_ID?enablejsapi=1" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 mt-3 mt-lg-0 px-0">
+                <h2 class="text-center py-3 fw-bolder" style="background: url(<?= get_stylesheet_directory_uri(); ?>/assets/src/images/background-gold.png);background-position: center;">YÊU CẦU TƯ VẤN </h2>
+                <form action=""></form>
+            </div>
+        </div>
+    </div>
+</div>
